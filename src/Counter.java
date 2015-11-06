f5d151a5019b3d68fcb2b1d658ec0be797a463c8
Rad Zajaczkowski <rad@mira-labs.com>
2015-11-06 10:21:10 +0000

    Removed try catch blocks

--- src/Counter.java
+++ src/Counter.java
@@ -7,24 +7,21 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Counter {
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner input = null;
         double sum = 0;
-        try {
+
             File file = new File("numbers.txt");
             //This is the entire logic - not using arrays
             input = new Scanner(file);
+
             while (input.hasNextDouble()) {
                 sum += input.nextDouble();
             }
             // output results in the correct way
             System.out.printf("The total sum of the "
                     + "doubles in the input file is %f\n", sum);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } finally {
-            input.close();
-        }
+
     }
 
 
